385b70e1781b28a001d61118a55ae5b93e8eb87e
Mike Echevarria <mechevarria@m1pro-echevarria.local>
2023-09-30 12:23:46 -0400

    initial setup for local MySQL database

---
+++ README.md
@@ -0,0 +1,11 @@
+# MySQL Sample
+
+* Start a MySQL database with the `./run-mysql.sh` script. Defaults
+```properties
+name=mysql
+rootpass=mysqlroot
+user=user
+userpass=userpass
+```
+
+* Connect via [DBeaver](https://dbeaver.io/download/) on `http://localhost:3306`. Make sure the connection parameter `allowPublicKeyRetrieval=true` is set
---
+++ run-mysql.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+name=mysql
+rootpass=mysqlroot
+user=user
+userpass=userpass
+
+if [[ ! -d "volume" ]]; then
+    echo "creating mysql data volume"
+    mkdir volume
+fi
+
+if [ $( docker ps -a | grep ${name} | wc -l ) -gt 0 ]; then
+    echo "${name} exists"
+    docker start ${name}
+else
+    echo "${name} does not exist"
+    docker run -d \
+    --name ${name} \
+    -p 3306:3306 \
+    -v ${PWD}/volume:/var/lib/mysql \
+    -e MYSQL_DATABASE=loan \
+    -e MYSQL_ROOT_PASSWORD=${rootpass} \
+    -e MYSQL_USER=${user} \
+    -e MYSQL_PASSWORD=${userpass} \
+    mysql
+fi
